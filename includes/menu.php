daf9994fe7983882618ec3ed24982cdd21a73883
John Meyer <jack10042@gmail.com>
2018-07-03 00:26:40 -0400

    Migrated to php includes heirarchy

--- includes/menu.php
+++ includes/menu.php
@@ -1,30 +1,6 @@
-<!doctype html>
-<html lang="en">
-    <head>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-
-        <title>Cincinnati Dance and Movement Center</title>
-
-        <style>
-            body{
-                background-color:aliceblue;
-                font-family:  serif;
-            }
-            .dropdown-item:active {
-                background-color:bisque !important;
-            }
-
-        </style>
-    </head>
-    <body>
         <nav class="navbar navbar-expand-lg navbar-light" style="background-color:cornsilk;">
 
-            <a href="#" class="navbar-brand" style="height: 50px"><img src="logo.jpg" style="max-height:140%; margin:-7px;"></a>
+            <a href="#" class="navbar-brand" style="height: 50px"><img src="/img/logo.jpg" style="max-height:140%; margin:-7px;"></a>
 
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -74,13 +50,3 @@
                 </ul>
             </div>
         </nav>
-
-        <h1>Cincinnati Dance and Movement Center</h1>
-
-        <!-- Optional JavaScript -->
-        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-    </body>
-</html>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <?php include_once 'includes/head.php'; ?>
+    </head>
+    <body>
+        <?php include_once 'includes/menu.php'; ?>
+        <h1>Cincinnati Dance and Movement Center</h1>
+
+	<?php include_once 'includes/javascript.php'; ?>
+    </body>
+</html>
