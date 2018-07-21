2cbd99525323da75f4aec47197d7ab0ad0733141
Keith Meyer <oxymoron0042@gmail.com>
2018-07-21 13:46:01 -0400

    Pre-Merge Update

---
+++ gallery.php
@@ -0,0 +1,39 @@
+<?php
+set_include_path('../');
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+	<?php include_once 'includes/head.php'; ?>
+
+	<link rel="stylesheet" href="css/carousel.css">
+	<link rel="stylesheet" href="css/awards.css">
+	<link rel="stylesheet" href="css/actioncalls.css">
+</head>
+
+<body>
+	<?php include_once 'includes/menu.php'; ?>
+
+	<div class="drop-up">
+		<div class="container-fluid body-container">
+			<div class="body-inner">
+				<div class="row justify-content-md-center">
+					<div class="col-md-10 col-md-offset-1">
+						<br>
+
+                        <p> Hello content </p>
+
+						<br>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<?php include_once 'includes/footer.php'; ?>
+
+	<?php include_once 'includes/javascript.php'; ?>
+</body>
+
+</html>
