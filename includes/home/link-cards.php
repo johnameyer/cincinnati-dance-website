b93f99c15be9a7168acbcf54653dd33e8b35afae
Keith Meyer <oxymoron0042@gmail.com>
2018-07-29 17:57:47 -0400

    Home Update

---
+++ includes/home/link-cards.php
@@ -0,0 +1,32 @@
+<div id="awards" class="row">
+    <div class="col-md">
+        <div class="card">
+            <img class="card-img-top" src="http://via.placeholder.com/200x200" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">#3 Dance Studio</h5>
+                <p class="card-text">Ranked #3 for dance studios in Cincinnati in 2017, and has ranked in the top consistently since 2011.</p>
+                <!--a href="#" class="btn btn-primary">Go somewhere</a-->
+            </div>
+        </div>
+    </div>
+    <div class="col-md">
+        <div class="card">
+            <img class="card-img-top" src="http://via.placeholder.com/200x200" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">A+ BBB Rating</h5>
+                <p class="card-text">Rated A+ with the Better Business Bureau for 17 years straight.</p>
+                <!--a href="#" class="btn btn-primary">Go somewhere</a-->
+            </div>
+        </div>
+    </div>
+    <div class="col-md">
+        <div class="card">
+            <img class="card-img-top" src="http://via.placeholder.com/200x200" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">National Dance Champs (2012)</h5>
+                <p class="card-text">Lorem Ipsum and description goes here</p>
+                <!--a href="#" class="btn btn-primary">Go somewhere</a-->
+            </div>
+        </div>
+    </div>
+</div>
--- includes/menu.php
+++ includes/menu.php
@@ -11,73 +11,74 @@
 	</a>
 
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-	aria-expanded="false" aria-label="Toggle navigation">
-	<span class="navbar-toggler-icon"></span>
-</button>
+			aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
 
-<div class="collapse navbar-collapse" id="navbarSupportedContent">
-	<ul class="navbar-nav mr-auto">
-		<li class="nav-item dropdown">
-			<a class="nav-link dropdown-toggle" href="#" id="dropdownAbout" role="button" data-toggle="dropdown" aria-haspopup="true"
-			aria-expanded="false">
-			About Us
-		</a>
-		<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-			<a class="dropdown-item" style="display:none" href="about/story">Our Story</a>
-			<a class="dropdown-item" href="about/crew">Our Faculty/Staff</a>
-			<a class="dropdown-item" href="about/studio">Our Facility</a>
-		</div>
-	</li>
-	<li class="nav-item">
-		<a class="nav-link" style="display:none" href="events/">News and Events</a>
-	</li>
-	<li class="nav-item dropdown" style="display:none">
-		<a class="nav-link dropdown-toggle" href="#" id="dropdownTeams" role="button" data-toggle="dropdown" aria-haspopup="true"
-		aria-expanded="false">
-		Programs
-	</a>
-	<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-		<a class="dropdown-item" href="team/ballet">Ballet Program</a>
-		<a class="dropdown-item" href="team/open">Open Division</a>
-		<a class="dropdown-item" href="team/performance">Performance Teams</a>
-	</div>
-</li>
-<li class="nav-item dropdown">
-	<a class="nav-link dropdown-toggle" href="#" id="dropdownTeams" role="button" data-toggle="dropdown" aria-haspopup="true"
-	aria-expanded="false">
-	2018-2019 Classes
-</a>
-<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-	<a class="dropdown-item" href="classes/">All</a>
-	<div class="dropdown-divider"></div>
-	<a class="dropdown-item" href="classes/preschool">Preschool (ages 2 to 6)</a>
-	<a class="dropdown-item" href="classes/ballet">Ballet & Pointe (6 to adult)</a>
-	<a class="dropdown-item" href="classes/tap">Tap (6 to adult)</a>
-	<a class="dropdown-item" href="classes/jazz-hip-hop">Jazz &amp; Hip-Hop (5 to adult)</a>
-	<a class="dropdown-item" href="classes/contemporary-acro-dance">Contemporary &amp; Acro-Dance (10 to adult)</a>
-	<a class="dropdown-item" href="classes/performance-teams">Performance Teams (8 to adult)</a>
-	<a class="dropdown-item" href="classes/drop-in">Drop-In Classes (6 to adult)</a>
-</div>
-</li>
-<li class="nav-item dropdown">
-	<a class="nav-link dropdown-toggle" href="#" id="dropdownTeams" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Student Info</a>
-	<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-		<a class="dropdown-item" href="student/tuition.php">Tuition/Fees</a>
-		<a class="dropdown-item" href="student/calendar.php">Calendar</a>
-	</div>
-</li>
-<li class="nav-item">
-	<a class="nav-link" href="gallery">Gallery</a>
-</li>
-</ul>
-<ul class="nav justify-content-end">
-	<?php if(isset($_SESSION['contact-id'])): ?>
-		<li class="nav-item justify-content-end" style="display: flex; align-items: center;">
-			Welcome <?php echo $_SESSION['fname']; ?>
-		</li>
-	<?php endif; ?>
-	<li class="nav-item">
-		<?php if(isset($_SESSION['contact-id'])): ?>
+	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul class="navbar-nav mr-auto">
+			<li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="#" id="dropdownAbout" role="button" data-toggle="dropdown" aria-haspopup="true"
+				   aria-expanded="false">
+					About Us
+				</a>
+				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+					<a class="dropdown-item" style="display:none" href="about/story">Our Story</a>
+					<a class="dropdown-item" href="about/why">Why CDMC</a>
+					<a class="dropdown-item" href="about/crew">Our Faculty/Staff</a>
+					<a class="dropdown-item" href="about/studio">Our Facility</a>
+				</div>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" style="display:none" href="events/">News and Events</a>
+			</li>
+			<li class="nav-item dropdown" style="display:none">
+				<a class="nav-link dropdown-toggle" href="#" id="dropdownTeams" role="button" data-toggle="dropdown" aria-haspopup="true"
+				   aria-expanded="false">
+					Programs
+				</a>
+				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+					<a class="dropdown-item" href="team/ballet">Ballet Program</a>
+					<a class="dropdown-item" href="team/open">Open Division</a>
+					<a class="dropdown-item" href="team/performance">Performance Teams</a>
+				</div>
+			</li>
+			<li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="#" id="dropdownTeams" role="button" data-toggle="dropdown" aria-haspopup="true"
+				   aria-expanded="false">
+					2018-2019 Classes
+				</a>
+				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+					<a class="dropdown-item" href="classes/">All</a>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item" href="classes/preschool">Preschool (ages 2 to 6)</a>
+					<a class="dropdown-item" href="classes/ballet">Ballet & Pointe (6 to adult)</a>
+					<a class="dropdown-item" href="classes/tap">Tap (6 to adult)</a>
+					<a class="dropdown-item" href="classes/jazz-hip-hop">Jazz &amp; Hip-Hop (5 to adult)</a>
+					<a class="dropdown-item" href="classes/contemporary-acro-dance">Contemporary &amp; Acro-Dance (10 to adult)</a>
+					<a class="dropdown-item" href="classes/performance-teams">Performance Teams (8 to adult)</a>
+					<a class="dropdown-item" href="classes/drop-in">Drop-In Classes (6 to adult)</a>
+				</div>
+			</li>
+			<li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="#" id="dropdownTeams" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Student Info</a>
+				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+					<a class="dropdown-item" href="student/tuition.php">Tuition/Fees</a>
+					<a class="dropdown-item" href="student/calendar.php">Calendar</a>
+				</div>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="gallery">Gallery</a>
+			</li>
+		</ul>
+		<ul class="nav justify-content-end">
+			<?php if(isset($_SESSION['contact-id'])): ?>
+			<li class="nav-item justify-content-end" style="display: flex; align-items: center;">
+				Welcome <?php echo $_SESSION['fname']; ?>
+			</li>
+			<?php endif; ?>
+			<li class="nav-item">
+				<?php if(isset($_SESSION['contact-id'])): ?>
 			<li class="nav-item dropdown">
 				<a class="nav-link dropdown-toggle" href="#" id="dropdown-account" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Account Info</a>
 				<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
@@ -87,11 +88,11 @@
 				</div>
 			</li>
 			<?php else: ?>
-				<a class="btn btn-primary" href="account/sign-in.php">Sign In</a>
+			<a class="btn btn-primary" href="account/sign-in.php">Sign In</a>
 			<?php endif; ?>
-		</li>
-	</ul>
-</div>
+			</li>
+		</ul>
+	</div>
 </nav>
 <div id="spacer">
 
