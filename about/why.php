d4960a145430da1028c60f9c654c71fb56e6444f
Keith Meyer <oxymoron0042@gmail.com>
2018-07-29 23:21:08 -0400

    Why Page

--- about/why.php
+++ about/why.php
@@ -4,36 +4,90 @@ set_include_path('../');
 <!doctype html>
 <html lang="en">
 
-<head>
-	<?php include_once 'includes/head.php'; ?>
+	<head>
+		<?php include_once 'includes/head.php'; ?>
 
-	<link rel="stylesheet" href="css/carousel.css">
-	<link rel="stylesheet" href="css/awards.css">
-	<link rel="stylesheet" href="css/actioncalls.css">
-</head>
+		<link rel="stylesheet" href="css/carousel.css">
+		<link rel="stylesheet" href="css/awards.css">
+		<link rel="stylesheet" href="css/actioncalls.css">
+	</head>
 
-<body>
-	<?php include_once 'includes/menu.php'; ?>
+	<body>
+		<?php include_once 'includes/menu.php'; ?>
 
-	<div class="drop-up">
-		<div class="container-fluid body-container">
-			<div class="body-inner">
-				<div class="row justify-content-md-center">
-					<div class="col-md-10 col-md-offset-1">
-						<br>
+		<div class="drop-up">
+			<div class="container-fluid body-container">
+				<div class="body-inner">
+					<div class="row justify-content-md-center">
+						<div class="col-md-10 col-md-offset-1">
+							<br>
 
-                        <p> Hello content </p>
+							<h1>Why Us?</h1>
+							<div class="col-md-6" style="margin:auto; display:block">
+								<div class="card">
+									<img src="img/about/about.jpg" class="card-img" alt="facility picture">
+								</div>
+							</div>
+							<p>
+								The Cincinnati Dance and Movement Center offers a complete curriculum for students age 2 through
+								adult, beginner through advanced, with classes in Ballet, Pointe, Tap, Jazz, Hip-Hop, Contemporary,
+								Acro-Dance, Pre-School Dance, and more.
+								At CDMC, we take great care to provide age-appropriate and level-appropriate material in a supportive
+								learning environment. Our classes stress proper technique while developing a love of the art of dance.
+							</p>
+							<br>
+							<h4>The CDMC Advantages:</h4>
 
-						<br>
+							<ul>
+								<li>
+									In business since 1986, having provided quality dance education to thousands of students in
+									the Cincinnati area
+								</li>
+								<li>
+									Longtime member of the Better Business Bureau with an A+ rating
+								</li>
+								<li>
+									Caring, professional faculty with numerous regional and national dance awards for superior
+									teaching and choreography
+								</li>
+								<li>
+									State-of-the-art facility featuring large, fully-equipped dance rooms with double ballet barres,
+									full dance mirrors, drinking fountains, and custom-built dance floors designed to reduce stress
+									on dancers’ joints
+								</li>
+								<li>
+									Small class sizes for more individual attention
+								</li>
+								<li>
+									A wide range of class options and class days/times from which to choose
+								</li>
+								<li>
+									Reasonably priced weekly classes, ranging from $40 per month to $60 per month, with
+									additional discounts including sibling discounts and multiple class discounts
+								</li>
+								<li>
+									Full-time office manager available during class time to answer questions, and well-organized
+									administrative operations
+								</li>
+								<li>
+									Various performance opportunities available to students, including our performance teams:
+									the Cincinnati Dance Crew, the Cincinnati Ballet Ensemble, and the Cincinnati Show Choir
+								</li>
+								<li>
+									Special events, including guest teachers and speakers, audition opportunities, theme weeks,
+									studio parties, annual student choreography showcase, and more!
+								</li>
+							</ul>
+							<br>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
 
-	<?php include_once 'includes/footer.php'; ?>
+		<?php include_once 'includes/footer.php'; ?>
 
-	<?php include_once 'includes/javascript.php'; ?>
-</body>
+		<?php include_once 'includes/javascript.php'; ?>
+	</body>
 
 </html>
