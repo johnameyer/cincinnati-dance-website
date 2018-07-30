e47c9a1c08ee8cd85086159559681f55529fd48a
Keith Meyer <oxymoron0042@gmail.com>
2018-07-30 00:01:01 -0400

    Social Buttons

--- includes/home/social-buttons.php
+++ includes/home/social-buttons.php
@@ -1,32 +1,11 @@
-<div id="awards" class="row">
-    <div class="col-md">
-        <div class="card">
-            <img class="card-img-top" src="http://via.placeholder.com/200x200" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">#3 Dance Studio</h5>
-                <p class="card-text">Ranked #3 for dance studios in Cincinnati in 2017, and has ranked in the top consistently since 2011.</p>
-                <!--a href="#" class="btn btn-primary">Go somewhere</a-->
-            </div>
-        </div>
-    </div>
-    <div class="col-md">
-        <div class="card">
-            <img class="card-img-top" src="http://via.placeholder.com/200x200" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">A+ BBB Rating</h5>
-                <p class="card-text">Rated A+ with the Better Business Bureau for 17 years straight.</p>
-                <!--a href="#" class="btn btn-primary">Go somewhere</a-->
-            </div>
-        </div>
-    </div>
-    <div class="col-md">
-        <div class="card">
-            <img class="card-img-top" src="http://via.placeholder.com/200x200" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">National Dance Champs (2012)</h5>
-                <p class="card-text">Lorem Ipsum and description goes here</p>
-                <!--a href="#" class="btn btn-primary">Go somewhere</a-->
-            </div>
-        </div>
-    </div>
+<div id="socials" class="row">
+	<div class="col-md-5" style="margin:auto; display:block">
+	<div class="card">
+		<div class="card-body">
+			<h5 class="card-title">Like us on Facebook!</h5>
+			<p class="card-text" style="display:inline">Visit our page for more pictures and the latest news.</p>
+			<a href="#" class="btn btn-primary" style="float:right; display:inline">Check It Out »</a>
+		</div>
+	</div>
+	</div>
 </div>
--- index.php
+++ index.php
@@ -1,38 +1,38 @@
 <!doctype html>
 <html lang="en">
 
-    <head>
-        <?php include_once 'includes/head.php'; ?>
+	<head>
+		<?php include_once 'includes/head.php'; ?>
 
-        <link rel="stylesheet" href="css/carousel.css">
-        <link rel="stylesheet" href="css/awards.css">
-        <link rel="stylesheet" href="css/actioncalls.css">
-    </head>
+		<link rel="stylesheet" href="css/carousel.css">
+		<link rel="stylesheet" href="css/awards.css">
+		<link rel="stylesheet" href="css/actioncalls.css">
+	</head>
 
-    <body>
-        <?php include_once 'includes/menu.php'; ?>
+	<body>
+		<?php include_once 'includes/menu.php'; ?>
 
-        <div style="height: 475px; position: relative">
+		<div style="height: 475px; position: relative">
 
-            <?php include_once 'includes/home/gallery-header.php'; ?>
+			<?php include_once 'includes/home/gallery-header.php'; ?>
 
-        </div>
+		</div>
 
-        <div class="drop-up">
-            <div class="container-fluid body-container">
-                <div class="body-inner">
-                    <div class="justify-content-md-center">
-                            <?php include_once 'includes/info.php'; ?>
+		<div class="drop-up">
+			<div class="container-fluid body-container">
+				<div class="body-inner">
+					<div class="justify-content-md-center">
+						<?php include_once 'includes/info.php'; ?>
+						<?php include_once 'includes/calendar.php'; ?>
+						<?php include_once 'includes/home/social-buttons.php'; ?>
+					</div>
+				</div>
+			</div>
+		</div>
 
-                            <?php include_once 'includes/calendar.php'; ?>
-                        </div>
-                </div>
-            </div>
-        </div>
-        
-        <?php include_once 'includes/footer.php'; ?>
+		<?php include_once 'includes/footer.php'; ?>
 
-        <?php include_once 'includes/javascript.php'; ?>
-    </body>
+		<?php include_once 'includes/javascript.php'; ?>
+	</body>
 
 </html>
