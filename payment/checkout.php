7b5291c09de705fd5fd830be75c5d2a34918c852
johnameyer <jack10042@gmail.com>
2018-07-31 19:34:15 -0400

    Updated paypal button for production

--- payment/checkout.php
+++ payment/checkout.php
@@ -81,16 +81,15 @@ $page = "Your Unpaid Classes";
 					<br>
 
 					<div>
-						<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
+						<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 							<input type="hidden" name="cmd" value="_s-xclick">
-							<input type="hidden" name="hosted_button_id" value="2QX8BND8KZ55C">
+							<input type="hidden" name="hosted_button_id" value="W8SNFPZ7ZZ2AL">
 							<input type="hidden" name="item_name" value="Class Registration">
 							<input type="hidden" name="quantity" value="<?php echo count($students); ?>">
 							<input type="hidden" name="custom" value="<?php echo $id; ?>">
-							<input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-							<img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+							<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+							<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 						</form>
-
 					</div>
 				</div>
 			</div>
