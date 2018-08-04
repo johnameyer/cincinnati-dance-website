4e6dca3c532f0c9065fadfbf412c4d422c5745b2
Keith Meyer <oxymoron0042@gmail.com>
2018-08-04 14:44:03 -0400

    Delete Conditionals

--- backend/email.php
+++ backend/email.php
@@ -1,11 +1,8 @@
 <?php
 
 function mailTo($email, $template_file){
-	try{
-		$headers = "MIME-Version: 1.0" . "\r\n";
-		$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-		$headers .= 'From: <noreply@cincinnatidance.com>' . "\r\n";
-		mail($email, 'Welcome to Cincinnati Dance', file_get_contents($template_file), $headers, '');
-	} catch(Exception $e){
-	}
+	$headers = "MIME-Version: 1.0" . "\r\n";
+	$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+	$headers .= 'From: <noreply@cincinnatidance.com>' . "\r\n";
+	@mail($email, 'Welcome to Cincinnati Dance', file_get_contents($template_file), $headers, '');
 }
