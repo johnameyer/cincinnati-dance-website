8369fdb3fa5f8e66eafe3791d75ff867ee4a0a56
johnameyer <jack10042@gmail.com>
2018-08-04 16:17:40 -0400

    Little changes

--- backend/delete-student-class.php
+++ backend/delete-student-class.php
@@ -4,10 +4,8 @@ include_once 'includes/db.php';
 
 header('Content-Type: application/json');
 
-$_SESSION['contact-id']
-
-
-if(isset($_SESSION['email']) && $_REQUEST['has_paid'] == 0){
+if(isset($_SESSION['contact-id']) && $_REQUEST['has_paid'] == 0){
+	//TODO check if contact has access to this student / student_class
 	$error = deleteStudentClass($_REQUEST['student'], $_REQUEST['class']);
 	if($error) echo json_encode($error);
 	else echo json_encode('success');
